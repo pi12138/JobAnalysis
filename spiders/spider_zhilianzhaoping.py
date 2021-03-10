236f7f2d0e278903cc73ccc1f3fba76848654a2e
zyp <1558255789@qq.com>
2021-03-10 22:55:00 +0800

    feat: 智联 spider

--- spiders/spider_zhilianzhaoping.py
+++ spiders/spider_zhilianzhaoping.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import logging
+import time
 
 logging.basicConfig(
     level=logging.INFO,
@@ -19,42 +20,65 @@ url = 'https://fe-api.zhaopin.com/c/i/search/positions?at=f5fd0fcc7cd54faa956be8
 
 
 class ZhiLianSpider:
-    def __init__(self):
+    def __init__(self, keyword='Java开发'):
         self.headers = headers
         self.url = url
+        self.count = 0
+        self.data_list = list()
+        self.end = False
+        self.job_list = list()
+        self.keyword = keyword
+        self.page_index = 1
 
-    def get_data(self, keyword='Java开发'):
+    def get_data(self):
         data = {
-            "S_SOU_FULL_INDEX": keyword,
+            "S_SOU_FULL_INDEX": self.keyword,
             "S_SOU_WORK_CITY": "653",  # 653代表杭州
             "pageSize": 30,
-            "pageIndex": 1,
+            "pageIndex": self.page_index,
             "cvNumber": "JI301770812R90500000000",
             "eventScenario": "pcSearchedSouSearch"
         }
 
         response = requests.post(self.url, json=data, headers=self.headers)
         if response.status_code == 200 and response.json()['code'] == 200:
-            result = response.json()['data']['list']
-            job_list = list()
-            for res in result:
-                job_info = {
-                    'company_info': res.get('companyName'),
-                    'name': res.get('name'),
-                    'location': '{}-{}-{}'.format(res.get('workCity'), res.get('cityDistrict'), res.get('tradingArea')),
-                    'education': res.get('education'),
-                    'salary': res.get('salary60'),
-                    'work_experience': res.get('workingExp'),
-                    'skill_label': [i.get('value') for i in res.get('skillLabel')],
-                    'welfare_label': [i.get('value') for i in res.get('welfareLabel')],
-                }
-                job_list.append(job_info)
-            with open('./data/智联_{}.json'.format(data['S_SOU_FULL_INDEX']), 'w') as f:
-                f.write(json.dumps(job_list, ensure_ascii=False))
-            logging.info('save success.')
+            result = response.json()
+            self.count = result['data']['count']
+            self.data_list.extend(result['data']['list'])
+            self.end = result['data']['isEndPage'] == 1
+            logger.info(f"data list extend: {result['data']['list']}")
         else:
-            logging.info('download failed.')
+            logger.error('download failed.')
+
+    def parse_data(self):
+        for res in self.data_list:
+            job_info = {
+                'company_info': res.get('companyName'),
+                'name': res.get('name'),
+                'location': '{}-{}-{}'.format(res.get('workCity'), res.get('cityDistrict'), res.get('tradingArea')),
+                'education': res.get('education'),
+                'salary': res.get('salary60'),
+                'work_experience': res.get('workingExp'),
+                'skill_label': [i.get('value') for i in res.get('skillLabel')],
+                'welfare_label': [i.get('value') for i in res.get('welfareLabel')],
+            }
+            self.job_list.append(job_info)
+
+    def download_data_to_file(self):
+        while not self.end:
+            self.get_data()
+            self.page_index += 1
+            time.sleep(1)
+
+        logger.info(f'download data count: {self.count}, len: {len(self.data_list)}')
+        self.parse_data()
+
+        with open(f'./data/智联_{self.keyword}.json', 'w') as f:
+            f.write(json.dumps(self.job_list, ensure_ascii=False))
+        logger.info(f'save success. len: {self.job_list}')
 
 
 if __name__ == "__main__":
-    logger.info('xxx')
+    # logger.info('xxx')
+    spider = ZhiLianSpider()
+    spider.download_data_to_file()
